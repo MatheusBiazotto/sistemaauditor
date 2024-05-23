7deaee64b298dda32a4f38196d2f3d3d9bd19536
Matheus Biazotto <83847093+MatheusBiazotto@users.noreply.github.com>
2024-05-23 23:58:40 +0000

    Formulário Produtos agora é funcional

--- components/formCliente/page.js
+++ components/formCliente/page.js
@@ -1,12 +1,15 @@
 "use client";
 
 import { useState } from "react";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 import { Input } from "@nextui-org/react";
 import { Button } from "@nextui-org/react";
 import { Tabs, Tab } from "@nextui-org/tabs";
 import { Select, SelectItem } from "@nextui-org/react";
 import { FaTrash } from "react-icons/fa6";
 import { FaRegEdit } from "react-icons/fa";
+import {Modal, ModalContent, ModalHeader, ModalBody, ModalFooter, Button, useDisclosure} from "@nextui-org/react";
 import {
   Table,
   TableHeader,
@@ -24,16 +27,31 @@ import { FaFilePdf } from "react-icons/fa6";
 
 export default function FormCliente() {
   const [isFormCliente, setIsFormCliente] = useState(true);
-  const [isFormProduct, setIsForProduct] = useState(false);
+  const [isFormProduct, setIsFormProduct] = useState(false);
+
+  //Info Clientes
+  const [cnpj, setCnpj] = useState("");
+  const [nomeFantasia, setNomeFantasia] = useState("");
+  const [email, setEmail] = useState("");
+  const [nomeCliente, setNomeCliente] = useState("");
+
+  //Info Produtos
   const [marcaProduto, setMarcaProduto] = useState("");
-  const [qtdFolhasRolos, setQtdFolhasRolos] = useState("");
-  const [qtdFardos, setQtdFardos] = useState("");
-  
+  const [qtdFolhasRolos, setQtdFolhasRolos] = useState(0);
+  const [qtdFardos, setQtdFardos] = useState(0);
+
+  //Metragens
+  const [diametroBobina, setDiametroBobina] = useState(0);
+  const [medidaBobina, setMedidaBobina] = useState(0);
+  const [larguraBobina, setLarguraBobina] = useState(0);
+  const [tipoFolha, setTipoFolha] = useState("");
 
   // para mobile
   const [isFormPapelToalha, setIsFormPapelToalha] = useState(false);
   const [isFormPapelHigienico, setIsFormPapelHigienico] = useState(false);
   const [isFormBobina, setIsFormBobina] = useState(false);
+  // fim mobile
+
   const columns = [
     {
       key: "tipoProduto",
@@ -56,18 +74,143 @@ export default function FormCliente() {
       label: "AÇÕES",
     },
   ];
-  const rows = [];
+  const [rows, setRows] = useState([]);
 
   const itemSelect = ["Folha simples", "Folha dupla"];
 
+  const cnpjFormat = (value) => {
+    value.replace(/\D/g, "");
+    return value
+      .replace(/\D/g, "")
+      .replace(/(\d{2})(\d)/, "$1.$2")
+      .replace(/(\d{3})(\d)/, "$1.$2")
+      .replace(/(\d{3})(\d)/, "$1/$2")
+      .replace(/(\d{4})(\d)/, "$1-$2")
+      .replace(/(-\d{2})\d+?$/, "$1");
+  };
+
   function nextForm() {
     setIsFormCliente(false);
-    setIsForProduct(true);
+    setIsFormProduct(true);
   }
 
   function backForm() {
     setIsFormCliente(true);
-    setIsForProduct(false);
+    setIsFormProduct(false);
+  }
+
+  function handleMarcaProduto(e) {
+    setMarcaProduto(e.target.value);
+  }
+
+  function handleQtdFolhasRolos(e) {
+    setQtdFolhasRolos(e.target.value);
+  }
+
+  function handleQtdFardos(e) {
+    setQtdFardos(e.target.value);
+  }
+
+  function handleDiametroBobina(e) {
+    setDiametroBobina(Number(e.target.value));
+  }
+
+  function handleMedidaBobina(e) {
+    setMedidaBobina(Number(e.target.value));
+  }
+
+  function handleLarguraBobina(e) {
+    setLarguraBobina(Number(e.target.value));
+  }
+
+  function handleTipoFolha(e) {
+    setTipoFolha(e.target.value);
+  }
+
+  function handleCNPJChange(e) {
+    setCnpj(cnpjFormat(e.target.value));
+  }
+
+  function handleNomeFantasiaChange(e) {
+    setNomeFantasia(e.target.value);
+  }
+
+  function handleEmailChange(e) {
+    setEmail(e.target.value);
+  }
+
+  function handleNomeClienteChange(e) {
+    setNomeCliente(e.target.value);
+  }
+
+  function saveProduct(e) {
+    console.log(e.target.name);
+    if (!marcaProduto || !qtdFolhasRolos || !qtdFardos) {
+      toast.warning("Preencha todos os campos para salvar o produto.", {
+        position: "bottom-right",
+        theme: "dark",
+      });
+      return;
+    }
+    let tipoProduto = "";
+    let haveMetragens = false;
+
+    if (e.target.name === "papelHigienico") {
+      tipoProduto = "Papel Higiênico Rolão";
+    } else if (e.target.name === "papelToalha") {
+      tipoProduto = "Papel Toalha Interfolhado";
+    } else if (e.target.name === "bobinaPapel") {
+      tipoProduto = "Bobina de Papel Toalha";
+      haveMetragens = true;
+    }
+
+    const lastKey = rows[rows.length - 1]?.key || 0;
+    const rowsAntiga = JSON.parse(JSON.stringify(rows));
+
+    if (!haveMetragens) {
+      rowsAntiga.push({
+        key: lastKey + 1,
+        tipoProduto: tipoProduto,
+        marcaProduto: marcaProduto,
+        qtdFolhasRolos: qtdFolhasRolos,
+        qtdFardos: qtdFardos,
+        actions: true,
+      });
+    } else {
+      rowsAntiga.push({
+        key: lastKey + 1,
+        tipoProduto: tipoProduto,
+        marcaProduto: marcaProduto,
+        qtdFolhasRolos: qtdFolhasRolos,
+        qtdFardos: qtdFardos,
+        actions: true,
+        metragens: {
+          diametroBobina: diametroBobina,
+          medidaBobina: medidaBobina,
+          larguraBobina: larguraBobina,
+          tipoFolha: tipoFolha,
+        },
+      });
+
+      setDiametroBobina(0);
+      setMedidaBobina(0);
+      setLarguraBobina(0);
+      setTipoFolha("");
+    }
+
+    setRows(rowsAntiga);
+
+    setMarcaProduto("");
+    setQtdFolhasRolos(0);
+    setQtdFardos(0);
+
+    setIsFormProduct(false);
+    setIsFormCliente(true);
+
+    toast.success("Produto salvo com sucesso!", {
+      position: "bottom-right",
+      theme: "dark",
+    });
   }
 
   return (
@@ -77,20 +220,40 @@ export default function FormCliente() {
           <>
             <div className="grid grid-cols-[repeat(auto-fit,_minmax(0,_300px))] justify-center lg:grid-cols-2 gap-4 w-full p-4">
               <div className="flex flex-col gap-4 justify-center">
-                <h1 className="text-center text-2xl font-bold">
+                <h1 className="text-center text-white text-2xl font-bold">
                   Dados do Cliente
                 </h1>
-                <Input type="text" label="CNPJ" required />
-                <Input type="text" label="Nome Fantasia" required />
-                <Input type="email" label="Email" required />
                 <Input
+                  onChange={handleCNPJChange}
+                  value={cnpj}
+                  type="text"
+                  label="CNPJ"
+                  isRequired
+                />
+                <Input
+                  onChange={handleNomeFantasiaChange}
+                  value={nomeFantasia}
+                  type="text"
+                  label="Nome Fantasia"
+                  isRequired
+                />
+                <Input
+                  onChange={handleEmailChange}
+                  value={email}
+                  type="email"
+                  label="Email"
+                  isRequired
+                />
+                <Input
+                  onChange={handleNomeClienteChange}
+                  value={nomeCliente}
                   type="text"
                   label="Nome do Cliente que acompanhou"
-                  required
+                  isRequired
                 />
               </div>
               <div className="flex flex-col w-full items-center gap-4">
-                <h1 className="text-center text-2xl font-bold">
+                <h1 className="text-center text-white text-2xl font-bold">
                   Produtos cadastrados
                 </h1>
                 <Table
@@ -168,10 +331,10 @@ export default function FormCliente() {
           </>
         ) : (
           <>
-            <h1 className="text-center text-2xl font-bold">
+            <h1 className="text-center text-white text-2xl font-bold">
               Dados dos Produtos
             </h1>
-            <h2 className="text-center text-lg font-bold">
+            <h2 className="text-center text-white text-lg font-bold">
               Selecione abaixo o produto que deseja medir
             </h2>
             <Tabs
@@ -185,14 +348,20 @@ export default function FormCliente() {
                 title="Papel Toalha Interfolhado"
               >
                 <div className="hidden lg:flex items-center justify-center flex-col flex-wrap p-4 gap-4 w-full">
-                  <Input type="text" label="Marca do produto" />
                   <Input
+                    onChange={handleMarcaProduto}
                     type="text"
+                    label="Marca do produto"
+                  />
+                  <Input
+                    type="number"
                     className="w-full"
+                    onChange={handleQtdFolhasRolos}
                     label="Quantidade de folhas (padrão é 1000)"
                   />
                   <Input
-                    type="text"
+                    type="number"
+                    onChange={handleQtdFardos}
                     label="Quantidade de fardos entregues/comprados"
                   />
                   <div className="flex gap-4">
@@ -203,7 +372,12 @@ export default function FormCliente() {
                     >
                       Voltar
                     </Button>
-                    <Button className="w-full" color="primary">
+                    <Button
+                      onClick={saveProduct}
+                      className="w-full"
+                      color="primary"
+                      name="papelToalha"
+                    >
                       Salvar
                     </Button>
                   </div>
@@ -211,14 +385,23 @@ export default function FormCliente() {
               </Tab>
               <Tab className="w-full" key="music" title="Papel Higiênico Rolão">
                 <div className="flex items-center justify-center flex-col flex-wrap p-4 gap-4 w-full">
-                  <Input type="text" label="Marca do produto" />
                   <Input
+                    onChange={handleMarcaProduto}
+                    name="marcaPapelHigienico"
                     type="text"
+                    label="Marca do produto"
+                  />
+                  <Input
+                    type="number"
+                    onChange={handleQtdFolhasRolos}
+                    name="qtdRolosPapelHigienico"
                     className="w-full"
                     label="Quantidade de rolos especificada na embalagem"
                   />
                   <Input
-                    type="text"
+                    onChange={handleQtdFardos}
+                    type="number"
+                    name="qtdFardosPapelHigienico"
                     label="Quantidade de fardos entregue/comprado"
                   />
                   <div className="flex gap-4">
@@ -229,7 +412,12 @@ export default function FormCliente() {
                     >
                       Voltar
                     </Button>
-                    <Button className="w-full" color="primary">
+                    <Button
+                      onClick={saveProduct}
+                      name="papelHigienico"
+                      className="w-full"
+                      color="primary"
+                    >
                       Salvar
                     </Button>
                   </div>
@@ -241,24 +429,42 @@ export default function FormCliente() {
                 title="Bobina de Papel Toalha"
               >
                 <div className="flex items-center justify-center flex-col flex-wrap p-4 gap-4 w-full">
-                  <Input type="text" label="Marca do Produto" />
                   <Input
+                    onChange={handleMarcaProduto}
                     type="text"
+                    label="Marca do Produto"
+                  />
+                  <Input
+                    onChange={handleQtdFolhasRolos}
+                    type="number"
                     className="w-full"
                     label="Quantidade de Folhas (padrão é 1000)"
                   />
                   <Input
-                    type="text"
+                    type="number"
+                    onChange={handleQtdFardos}
                     label="Quantidade de Fardos entregue/comprado"
                   />
-                  <h2 className="text-center text-lg font-bold">Metragens</h2>
-                  <Input type="number" label="Diâmetro da bobina" />
+                  <h2 className="text-center text-white text-lg font-bold">
+                    Metragens
+                  </h2>
                   <Input
+                    onChange={handleDiametroBobina}
+                    type="number"
+                    label="Diâmetro da bobina"
+                  />
+                  <Input
+                    onChange={handleMedidaBobina}
                     type="number"
                     label="Medida da quantidade de papel na Bobina"
                   />
-                  <Input type="number" label="Largura da Bobina" />
+                  <Input
+                    onChange={handleLarguraBobina}
+                    type="number"
+                    label="Largura da Bobina"
+                  />
                   <Select
+                    onChange={handleTipoFolha}
                     label="Folha simples ou dupla"
                     className="w-full text-black"
                   >
@@ -280,7 +486,12 @@ export default function FormCliente() {
                     >
                       Voltar
                     </Button>
-                    <Button className="w-full" color="primary">
+                    <Button
+                      onClick={saveProduct}
+                      name="bobinaPapel"
+                      className="w-full"
+                      color="primary"
+                    >
                       Salvar
                     </Button>
                   </div>
@@ -319,6 +530,7 @@ export default function FormCliente() {
           </>
         )}
       </form>
+      <ToastContainer autoClose={5000} />
     </div>
   );
 }
